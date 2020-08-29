78779f4733c3efdb4c60abfeb5aa507b449320d6
akmalsalem <akmalsalem@gmail.com>
2020-08-29 14:05:01 +0200

    Updates

---
+++ precache-manifest.551ce2d64f92eb11a1b4929e24e68b13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab2b89ddcb7817cf592691b3d40b8ae",
+    "url": "/ReactRecipes/index.html"
+  },
+  {
+    "revision": "b18af4037964c617cedb",
+    "url": "/ReactRecipes/static/css/main.b1dfa761.chunk.css"
+  },
+  {
+    "revision": "992ac23ea00c2cf581f0",
+    "url": "/ReactRecipes/static/js/2.93e8bea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRecipes/static/js/2.93e8bea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18af4037964c617cedb",
+    "url": "/ReactRecipes/static/js/main.17fe9550.chunk.js"
+  },
+  {
+    "revision": "dec71c58a75b47787cf8",
+    "url": "/ReactRecipes/static/js/runtime-main.951324ea.js"
+  },
+  {
+    "revision": "8cde6f3be6e0f7d417e0bab91b11f4c6",
+    "url": "/ReactRecipes/static/media/pexels-lukas-349610.8cde6f3b.jpg"
+  }
+]);
